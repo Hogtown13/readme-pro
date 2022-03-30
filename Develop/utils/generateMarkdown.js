9b82826fb78c904e07902d180e38832c18bc8c7d
Hogtown13 <tribenation13@yahoo.com>
2022-03-29 21:22:47 -0400

    added ! badge

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -6,9 +6,9 @@ function renderLicenseBadge(license) {
 
   let yourLicense = ''
   if (license=== 'MIT') {
-    yourLicense = `[license: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+    yourLicense = `![license: MIT](https://img.shields.io/badge/License-MIT-yellow)`
   } else if (license=== 'ISC') {
-    yourLicense = `[license: ISC](https://img.shields.io/badge/License-isc-yellow.svg)`
+    yourLicense = `![license: ISC](https://img.shields.io/badge/License-ISC-yellow)`
   } else if ( license === '') {
     yourLicense = `['']`;
   }
